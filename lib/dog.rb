947aadc3473371c583921dc3bf3f13e683fe52da
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-08-17 21:39:51 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  #Give a Dog a Name: #name= that write to a corresponding instance variable @name.
+  def name=(dog_name)
+    @name=dog_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed_name)
+    @breed=breed_name
+  end
+
+  def breed
+    @breed
+  end
+#Give a Dog a breed: #breed that read
+#Give a Dog a breed: #breed= that write to a corresponding instance variable @breed.
+end
---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+  #Give a Dog a Name: #name= that write to a corresponding instance variable @name.
+  def name=(person_name)
+    @name=person_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(job_name)
+    @job=job_name
+  end
+
+  def job
+    @job
+  end
+#Give a Dog a breed: #breed that read
+#Give a Dog a breed: #breed= that write to a corresponding instance variable @breed.
+end
