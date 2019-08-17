b0e32847288ac4b3b496c5bfb8f20e52cfa3bb98
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-08-17 20:51:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,13 @@
 class Dog
-#Give a Dog a Name: #name that read
-#Give a Dog a Name: #name= that write to a corresponding instance variable @name.
+  #Give a Dog a Name: #name that read
+  def name
+
+  end 
+
+  #Give a Dog a Name: #name= that write to a corresponding instance variable @name.
+  def name=("")
+
+  end 
 
 #Give a Dog a breed: #breed that read
 #Give a Dog a breed: #breed= that write to a corresponding instance variable @breed.
